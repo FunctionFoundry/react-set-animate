90c9b621b25c038e0a1dff7bb7be155a0bdf1359
Peter Moresi <peter.moresi@pnmac.com>
2015-10-29 13:40:43 -0700

    Fix test

--- test/sanity.js
+++ test/sanity.js
@@ -1,10 +1,12 @@
 var test = require('tape')
 var Animate = require('../build/index').Animate
 var AnimatedComponent = require('../build/index').AnimatedComponent
+var Eases = require('../build/index').Eases
 
 test('it should be there', function(t) {
-  t.plan(2)
+  t.plan(3)
   t.equals(typeof Animate, 'function' )
   t.equals(typeof AnimatedComponent, 'function' )
+  t.equals( typeof Eases, 'object')
 
 })
