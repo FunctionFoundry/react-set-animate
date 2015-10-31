3dddaff40b915e64a32ae0d9b1609f467fd2d761
Peter Moresi <peter.moresi@gmail.com>
2015-10-30 22:15:21 -0700

    update test

--- test/sanity.js
+++ test/sanity.js
@@ -1,18 +1,37 @@
-var test = require('tape')
-var Animate = require('../lib/index').Animate
-var AnimatedComponent = require('../lib/index').AnimatedComponent
-var Eases = require('../lib/index').Eases
+var test = require('tape');
+var Animate = require('../lib/index').Animate;
+var AnimatedComponent = require('../lib/index').AnimatedComponent;
+var Eases = require('../lib/index').Eases;
 
 test('it should be there', function(t) {
-  t.plan(5)
-  t.equals(typeof Animate, 'function' )
-  t.equals(typeof AnimatedComponent, 'function' )
-  t.equals( typeof Eases, 'object')
+  t.plan(9);
+
+  t.equals(typeof Animate, 'function' );
+  t.equals(typeof AnimatedComponent, 'function' );
+  t.equals( typeof Eases, 'object');
 
   var cmp = new AnimatedComponent();
-  console.log( "FOO" )
-  console.log( cmp )
-  t.equals( typeof cmp, 'object' )
-  t.deepEquals( Object.keys(cmp), [ 'props', 'context', 'refs', 'updater', 'animator', 'setAnimate', 'stopAnimate' ] )
+  var mockCmp = {
+    state: { x: 0 },
+    setState: function(props) {
+      this.state = Object.assign( this.state, props )
+    }
+  };
 
-})
+  var animate = new Animate(mockCmp);
+  t.equals( typeof animate, 'object' );
+  t.equals( typeof animate.tween, 'function' );
+
+  t.equals( typeof cmp, 'object' );
+  t.deepEquals( Object.keys(cmp),
+  [ 'props', 'context', 'refs', 'updater', 'animator', 'setAnimate', 'stopAnimate' ] );
+
+  animate.tween('x', 100, 10);
+
+  // check in 20 seconds to see if final value is realized!
+  setTimeout( function() {
+    t.equals( typeof mockCmp.state, 'object');
+    t.equals( mockCmp.state.x, 100);
+  }, 20);
+
+});
