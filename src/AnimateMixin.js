71344872165d97a906dde382197b97e3a1d450a8
Peter Moresi <peter.moresi@pnmac.com>
2016-02-22 14:22:17 -0800

    Try will mount

--- src/AnimateMixin.js
+++ src/AnimateMixin.js
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react'
 import {Animate} from './Animate'
 
 const AnimateMixin = {
-  componentDidMount() {
+  componentWillMount() {
     this.animator = new Animate(this)
     this.setAnimate = this.animator.tween.bind(this.animator)
   }
