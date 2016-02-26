a323272dbe02dcaf8e0613d40baef1e2cf2487d9
Peter Moresi <peter.moresi@gmail.com>
2016-02-26 08:51:00 -0800

    Add stopAnimate to Mixin

--- src/AnimateMixin.js
+++ src/AnimateMixin.js
@@ -5,6 +5,7 @@ const AnimateMixin = {
   componentWillMount() {
     this.animator = new Animate(this)
     this.setAnimate = this.animator.tween.bind(this.animator)
+    this.stopAnimate = this.animator.stop.bind(this.animator)
   }
 }
 
