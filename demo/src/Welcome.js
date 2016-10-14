457ad183d14c7903cdd328153522e77d214f67bd
Peter <peter.moresi@gmail.com>
2016-10-14 11:54:31 -0700

    Update tooling

--- demo/src/Welcome.js
+++ demo/src/Welcome.js
@@ -1,11 +1,18 @@
 import React, { PropTypes } from 'react'
+import ResumeContent from '../../content/resume.md'
+import Markdown from 'react-remarkable'
 
-const Welcome = React.createClass({
+const Resume = React.createClass({
   render () {
     return (
-      <div>Welcome</div>
+      <div className="wrapper">
+        <Markdown
+          source={ ResumeContent }
+          options={ markdownOptions }
+        />
+      </div>
     )
   }
 })
 
-export default Welcome
+export default Resume
