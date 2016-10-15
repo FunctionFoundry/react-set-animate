560acefb27dc26f3ce839cbfe9dc813e5b7df6e8
Peter <peter.moresi@gmail.com>
2016-10-15 08:16:36 -0700

    Drop

--- demo/public/dist/4.4.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([4],{223:function(e,a,t){"use strict";Object.defineProperty(a,"__esModule",{value:!0});var n,r=t(14);r.PropTypes;a["default"]=n=r.createClass({displayName:"PageNotFound",render:function(){return r.createElement("div",null,r.createElement("div",{className:"page-heading"},r.createElement("div",{className:"wrapper"},r.createElement("h1",null,"Page Not Found"))))}})}});
